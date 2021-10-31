0725ce13f86f8047d1376be76c53ae13ba3e9798
Momen Hamaveysi <momen.hamaveysi@kubakgroup.com>
2021-11-01 01:32:01 +0330

    Complete CreateLinkedList example

---
+++ src/com/datastructure/LinkedListExample.java
@@ -0,0 +1,41 @@
+package com.datastructure;
+
+public class LinkedListExample {
+
+    public void printExamples() {
+        Node first = methodA();
+        System.out.println("Method A");
+        traverse(first);
+
+        System.out.println("Method B");
+        first = methodB();
+        traverse(first);
+    }
+
+    private void traverse(Node first) {
+        if (first == null) return;
+
+        System.out.print(first.getValue() + "\t");
+        traverse(first.getNext());
+    }
+
+    private Node methodA() {
+        Node first = new Node(1);
+        first.setNext(new Node(2));
+        first.getNext().setNext(new Node(3));
+        first.getNext().getNext().setNext(new Node(4));
+        first.getNext().getNext().getNext().setNext(new Node(5));
+        first.getNext().getNext().getNext().getNext().setNext(new Node(6));
+        return first;
+    }
+
+    private Node methodB() {
+        Node first = new Node(1);
+        first = new Node(2, first);
+        first = new Node(3, first);
+        first = new Node(4, first);
+        first = new Node(5, first);
+        first = new Node(6, first);
+        return first;
+    }
+}
--- src/com/datastructure/Main.java
+++ src/com/datastructure/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 //        new BitWise().printExamples();
 
-        new DoubleAndFloatNumbersProblem().printExamples();
+//        new DoubleAndFloatNumbersProblem().printExamples();
+
+        new LinkedListExample().printExamples();
     }
 }
